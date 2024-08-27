54f18f225ed6ec10bdf7219e2ad64a5a95b3852a
mpolasub <nondhawit@gmail.com>
2024-08-27 01:15:05 -0700

    added contact page features

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 posts = requests.get(
@@ -17,14 +17,32 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        return receive_data()
+    else:
+        return get_contact_page()
 
 
 @app.route("/post/<int:pg_id>")
 def post(pg_id):
     return render_template("post.html", post_det=posts[pg_id])
+
+
+@app.post("/contact")
+def receive_data():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+    print(f"{name}\n{email}\n{phone}\n{message}")
+    return render_template("contact.html", pg_type=2)
+
+
+@app.get("/contact")
+def get_contact_page():
+    return render_template("contact.html", pg_type=1)
 
 
 if __name__ == "__main__":
