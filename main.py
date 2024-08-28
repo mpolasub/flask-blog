8149e72e214338c3772663d42373bc7523593df3
mpolasub <nondhawit@gmail.com>
2024-08-28 00:46:38 -0700

    added email sending to contacts page

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from email.message import EmailMessage
 
 posts = requests.get(
-    "https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
+    "https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw"
+    "/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
 
 app = Flask(__name__)
 
@@ -36,13 +39,30 @@ def receive_data():
     email = request.form['email']
     phone = request.form['phone']
     message = request.form['message']
-    print(f"{name}\n{email}\n{phone}\n{message}")
+    send_message(name, email, phone, message)
     return render_template("contact.html", pg_type=2)
 
 
 @app.get("/contact")
 def get_contact_page():
     return render_template("contact.html", pg_type=1)
+
+
+def send_message(name, email, phone, message):
+    login_email = ""
+    login_password = ""
+    msg = EmailMessage()
+    msg.set_content(f'From: {name} ({email}, {phone})\n\n{message}')
+
+    msg['Subject'] = 'New message from website!'
+    msg['From'] = email
+    msg['To'] = ""
+
+    # Send the message via our own SMTP server.
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(login_email, login_password)
+    server.send_message(msg)
+    server.quit()
 
 
 if __name__ == "__main__":
